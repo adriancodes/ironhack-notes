b2c304fdcdd5ff6ba79cf99d1ad203f49463702a
Adrian Martin <amartin@carecloud.com>
2016-07-14 17:45:58 -0400

    Added some examples
    Signed-off-by: Adrian Martin <amartin@carecloud.com>

--- scopesclojures/enumerable.js
+++ scopesclojures/enumerable.js
@@ -1,26 +1,59 @@
 'use strict'
 var coll = [1, 15, 27, 4, 35, 46];
+var l = (data) => console.log(data)
 
-for (var i = 0; i < coll.length; i++) {
-  console.log(coll[i]);
-}
 
-coll.forEach(function(value, index, arr) {
-  console.log(`index: ${index} value: ${value}`)
-})
+// for (var i = 0; i < coll.length; i++) {
+//   l(coll[i]);
+// }
 
-coll.map(function(item) {
-  console.log(item + 3);
-});
+// coll.forEach(function(value, index, arr) {
+//   console.log(`index: ${index} value: ${value}`)
+// })
 
-var filtered = coll.filter(function(item){
-  return item % 2 === 0;
-})
-console.log(filtered);
+// coll.map(function(item) {
+//   console.log(item + 3);
+// });
 
-var reduced = coll.reduce(function(previousValue, currentValue, currentIndex){
-  console.log(`previousValue: ${previousValue} currentValue: ${currentValue} currentIndex:  ${currentIndex}`);
-  console.log(`Total: ${previousValue + currentValue}`);
-  return previousValue + currentValue;
-});
-console.log(reduced);
+// var filtered = coll.filter(function(item){
+//   return item % 2 === 0;
+// })
+// console.log(filtered);
+
+// var reduced = coll.reduce(function(previousValue, currentValue, currentIndex){
+//   console.log(`previousValue: ${previousValue} currentValue: ${currentValue} currentIndex:  ${currentIndex}`);
+//   console.log(`Total: ${previousValue + currentValue}`);
+//   return previousValue + currentValue;
+// });
+// console.log(reduced);
+
+// // Recursion
+
+// var counter = 10;
+// while(counter > 0) {
+//   console.log(counter--);
+// }
+
+
+// var countdown = function(value) {
+//   if (value === 0) {
+//     return value;
+//   } else {
+//     console.log(value);
+//     return countdown(value - 1);
+//   }
+// };
+// countdown(10);
+
+// Factorial Example
+// Example: 4! is shorthand for 4 x 3 x 2 x 1
+// n! = n × (n−1)!
+
+// var factorial = function(number) {
+//   if (number <= 0) { 
+//     return 1;
+//   } else { 
+//     return (number * factorial(number - 1));
+//   }
+// };
+// console.log(factorial(6));
--- scopesclojures/examples.js
+++ scopesclojures/examples.js
@@ -1,44 +1,57 @@
 // Closures
 
-// Example 1
+// Closures
 
-function ticketBuilder(transport) {
-  var passengerNumber = 0;
-  return function(name){
-    passengerNumber++;
-    console.log("Welcome, " + name + ". Here is your ticket for the " + transport +
-                " You are passenger #" + passengerNumber + ".");
+var add = function(value1)  {
+  return function(value2) {
+    return value1 + value2
   };
 }
 
-
-var getPlainTicket = ticketBuilder("plane");
-var getTrainTicket = ticketBuilder("train");
-
-getPlainTicket("John Smith");
-getTrainTicket("Jane Smith");
-
-// Example 2
-
-function checkInPassenger(name, customersArray){
-  var passengerChecked;
-  customersArray.forEach(function(value, index) {
-    if (value === name) {
-      passengerChecked = function() {
-        console.log("Hi, " + name + ". You're passenger #" + (index + 1));
-      };
-    };
-  });
-  return passengerChecked;
-}
+var incrementBy3 = add(3)
+l(incrementBy3(10));
 
 
-var flightToBali = ["Wayan", "Putu", "Gede", "Ni Luh", "Nyoman"];
-var counterCheckIn = checkInPassenger("Gede", flightToBali);
-counterCheckIn();
+
+// // Example 1
+
+// function ticketBuilder(transport) {
+//   var passengerNumber = 0;
+//   return function(name){
+//     passengerNumber++;
+//     console.log("Welcome, " + name + ". Here is your ticket for the " + transport +
+//                 " You are passenger #" + passengerNumber + ".");
+//   };
+// }
 
 
-// Example 3
+// var getPlainTicket = ticketBuilder("plane");
+// var getTrainTicket = ticketBuilder("train");
+
+// getPlainTicket("John Smith");
+// getTrainTicket("Jane Smith");
+
+// // Example 2
+
+// function checkInPassenger(name, customersArray){
+//   var passengerChecked;
+//   customersArray.forEach(function(value, index) {
+//     if (value === name) {
+//       passengerChecked = function() {
+//         console.log("Hi, " + name + ". You're passenger #" + (index + 1));
+//       };
+//     };
+//   });
+//   return passengerChecked;
+// }
+
+
+// var flightToBali = ["Wayan", "Putu", "Gede", "Ni Luh", "Nyoman"];
+// var counterCheckIn = checkInPassenger("Gede", flightToBali);
+// counterCheckIn();
+
+
+// // Example 3
 
 // function checkInPassenger(name, customersArray){
 //   customersArray.forEach(function(value, index) {
@@ -55,13 +68,13 @@ counterCheckIn();
 // var counterCheckIn1 = checkInPassenger("Gede", flightToBali);
 // counterCheckIn1();
 
-console.log(declaredLater);
-var declaredLater = "Now it's defined!";
+// console.log(declaredLater);
+// var declaredLater = "Now it's defined!";
 
-isItHoisted()
-function isItHoisted(){
-  console.log("Yes!");
-}
+// isItHoisted()
+// function isItHoisted(){
+//   console.log("Yes!");
+// }
 
 // isNotHoisted();
 
